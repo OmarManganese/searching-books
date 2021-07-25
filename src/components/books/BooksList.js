d8783919f059393f3e633ea50cde9d7a8cb27731
OmarManganese <78893144+OmarManganese@users.noreply.github.com>
2021-07-25 11:04:16 +0200

    Unistalled dotenv, installed lodash and basic conditional render of the BookList component

--- src/components/books/BooksList.js
+++ src/components/books/BooksList.js
@@ -1,27 +1,39 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import _ from 'lodash';
 
 import Book from './Book';
 
 
 export default function BooksList() {
   const searchTerm = useSelector(state => state.books.searchTerm);
-  const books = useSelector(state => state.books.books);
+  const books = useSelector(state => _.get(state, "books.books.data.items", {}));
   const status = useSelector(state => state.books.status);
 
-  console.log(!books)
-
-  if (!searchTerm && !books) {
+  if (_.isEmpty(books) && !searchTerm) {
     return (
       <h2>Nulla</h2>
     )
   }
+
+  if (status === "loading") {
+    return (
+      <h2>Loading...</h2>
+    )
+  }
   
-  return (
-    <>
+  if (status === "failed") {
+    return (
+      <h2>Error</h2>
+    )
+  }
+  
+  if (status === "succeeded") {
+    return (
       <h2>Books</h2>
-    </>
-  )
+    )
+  }
+
 }
 
 
